2d851a3edff8d8e3b439d98e06cae3998f98b2ef
BigRedBot <bigredbot@users.noreply.github.com>
2017-11-30 23:29:09 -0500

    Beta

--- CreateBetaInstaller.cmd
+++ CreateBetaInstaller.cmd
@@ -2,4 +2,4 @@
 SET VERSION=10.26 Beta
 SET NAME=Neverwinter Invoke Bot
 SET INSTALLER=NeverwinterInvokeBotBeta
-CALL CompileInstaller.cmd "%VERSION%" "%NAME%" "%INSTALLER%" "Beta"
+CALL Installer.cmd "%VERSION%" "%NAME%" "%INSTALLER%" "Beta"
--- CreateInstaller.cmd
+++ CreateInstaller.cmd
@@ -2,4 +2,4 @@
 SET VERSION=10.25
 SET NAME=Neverwinter Invoke Bot
 SET INSTALLER=NeverwinterInvokeBot
-CALL CompileInstaller.cmd "%VERSION%" "%NAME%" "%INSTALLER%"
+CALL Installer.cmd "%VERSION%" "%NAME%" "%INSTALLER%"
