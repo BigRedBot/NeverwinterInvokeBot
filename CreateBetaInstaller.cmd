6b20889d110203b5f3304611c795c652d9392480
BigRedBot <bigredbot@users.noreply.github.com>
2017-12-02 16:06:51 -0500

    VIP Edit

--- CreateBetaInstaller.cmd
+++ CreateBetaInstaller.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET VERSION=10.25.3 Beta
+SET VERSION=10.25.4 Beta
 SET NAME=Neverwinter Invoke Bot
 SET INSTALLER=NeverwinterInvokeBotBeta
 CALL Installer.cmd "%VERSION%" "%NAME%" "%INSTALLER%" "Beta"
